f8512e1976f80b4294e12c1c50b454ee5bb3d822
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-11-22 19:26:03 -0500

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,1 +1,45 @@
+/*
+Big Apples (Weichen Liu + Blueface, Jeffery Tang + Mathias, Lior + Toothless)
+APCS
+LAB01 -- YoRPG
+2021-11-22
+time spent: .5 hours
+
+DISCO
+-
+
+QCC
+-
+
+
+*/
+
+public class Character {
+  public int health;
+  public int strength;
+  public int defense;
+  public double ar;
+
+  public boolean isAlive() {
+    return (health > 0);
+  }
+
+  public int getDefense() {
+    return defense; 
+  }
+
+  public void lowerHP(int damage) {
+    health -= damage; 
+  }
+
+  public int attack(Character character) {
+    int damage = (int)((strength * ar) - character.getDefense());
+    if (damage > 0) {
+      character.lowerHP(damage);
+    } else {
+      damage = 0;
+    }
+    return damage;
+  }
+}
 
