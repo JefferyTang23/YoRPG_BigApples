3327489c851edd5e04ba48e07b534d7cff7b9272
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-11-22 19:22:04 -0500

    Update Character.java

--- v1/Character.java
+++ v1/Character.java
@@ -1,8 +1,8 @@
 public class Character {
-  private int health;
-  private int strength;
-  private int defense;
-  private double ar;
+  public int health;
+  public int strength;
+  public int defense;
+  public double ar;
 
   public boolean isAlive() {
     return (health > 0);
