80620781ecc6a40a9d6e78539e396cfb1dedd547
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-11-22 19:51:38 -0500

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,17 +1,15 @@
 /*
 Big Apples (Weichen Liu + Blueface, Jeffery Tang + Mathias, Lior + Toothless)
 APCS
-LAB01 -- YoRPG
+lab01 -- Developing a RPG game
 2021-11-22
-time spent: .5 hours
+time spent: 0.8 hours
 
 DISCO
--
+- The instance variables in the super class cannot be private if you want to access them in the sub classes. 
 
 QCC
--
-
-
+- Are there other visibility types in Java?
 */
 
 public class Character {
