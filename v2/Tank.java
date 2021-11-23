2f69d04c773f54ea9fa3190decf9eeae73fdf28e
JefferyTang23 <jtang30@stuy.edu>
2021-11-23 12:33:19 -0500

    Tank

---
+++ v2/Tank.java
@@ -0,0 +1,13 @@
+/*
+Big Apples (Weichen Liu + Blueface, Jeffery Tang + Mathias, Lior + Toothless)
+APCS
+lab01 -- Developing a RPG game
+2021-11-22
+time spent: 0.8 hours
+*/
+
+public class Tank extends Protagonist {
+  public static String about() {
+    return "A warrior with a lot of defense."
+  }
+}
