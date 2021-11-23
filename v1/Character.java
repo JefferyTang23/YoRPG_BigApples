fdd40de14283470ed1ec19a205a9f326fa1df6fe
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-11-22 19:13:33 -0500

    Update Character.java

--- v1/Character.java
+++ v1/Character.java
@@ -1,1 +1,28 @@
+public class Character {
+  private int health;
+  private int strength;
+  private int defense;
+  private double ar;
 
+  public boolean isAlive() {
+    return (hp > 0);
+  }
+
+  public int getDefense() {
+    return defense; 
+  }
+
+  public void lowerHP(int damage) {
+    hp -= damage; 
+  }
+
+  public int attack(Character character) {
+    int damage = (int)((strength * ar) - character.getDefense());
+    if (damage > 0) {
+      character.bleed(damage);
+    } else {
+      damage = 0;
+    }
+    return damage;
+  }
+}
