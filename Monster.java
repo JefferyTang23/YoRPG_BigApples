c27d421a95a3a98c4cd06e30453292959ce862ab
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-11-22 19:26:25 -0500

    Update Monster.java

--- Monster.java
+++ Monster.java
@@ -1,1 +1,54 @@
+/*
+Big Apples (Weichen Liu + Blueface, Jeffery Tang + Mathias, Lior + Toothless)
+APCS
+LAB01 -- YoRPG
+2021-11-22
+time spent: .5 hours
+
+DISCO
+-
+
+QCC
+-
+
+
+*/
+
+public class Monster extends Character{
+
+  public Monster() {
+    health = 200;
+    defense = 10;
+    strength = (int) (Math.random() * 30 + 11);
+    ar = Math.random(); 
+  }
+
+
+  // accessing instance variables
+
+  public int getHp () {
+    return health;
+  }
+
+  public int getStrength() {
+    return strength;
+  }
+
+
+
+
+  // changing instance variables
+
+  public void setHP(int newHP) {
+    health = newHP;
+  }
+
+  public void setDefense(int newDefense) {
+    defense = newDefense;
+  }
+
+  public void setStrength(int newStrength) {
+    strength = newStrength;
+  }
+}
 
