f06d967932524c7b7eecc3e1dd5b87c1850674c5
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-11-23 19:12:30 -0500

    Create Warrior.java

---
+++ v2/Warrior.java
@@ -0,0 +1,21 @@
+/*
+Big Apples (Weichen Liu + Blueface, Jeffery Tang + Mathias, Lior + Toothless)
+APCS
+lab01 -- Developing a RPG game
+2021-11-22
+time spent: 0.8 hours
+*/
+
+public class Warrior extends Protagonist {
+  public static String about() {
+    return "A protagonist with a lot of strength and defense."
+  }
+  
+  public Tank() {
+    health = 300;
+    strength = 70;
+    defense = 40; 
+    ar = 0.5;
+    specialPower = 5; 
+  }
+}
