13ca3bbfc73e1a6e9ffc1b0afefc36e025c9441e
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-11-23 19:27:07 -0500

    Update Protagonist.java

--- v2/Protagonist.java
+++ v2/Protagonist.java
@@ -4,8 +4,13 @@ APCS
 lab01(v2) -- Developing subclasses for Protagonist and Monster
 2021-11-23
 time spent: 0.8 hours
-*/
 
+DISCO
+- The instance variables in the super class cannot be private if you want to access them in the sub classes. 
+
+QCC
+- Are there other visibility types in Java?
+*/
 public class Protagonist extends Character{
   protected String name;
   protected int specialPower;
