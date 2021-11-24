02c533cddb9d28ae3aa6945d9f7ff69a798c57e2
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-11-23 19:27:24 -0500

    Update Warrior.java

--- v2/Warrior.java
+++ v2/Warrior.java
@@ -4,6 +4,12 @@ APCS
 lab01(v2) -- Developing subclasses for Protagonist and Monster
 2021-11-23
 time spent: 0.8 hours
+
+DISCO
+- The instance variables in the super class cannot be private if you want to access them in the sub classes. 
+
+QCC
+- Are there other visibility types in Java?
 */
 
 public class Warrior extends Protagonist {
