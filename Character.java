066075a2d8ddff5149ff82f63bf46c435b1a4c3d
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-11-27 17:52:18 -0500

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,33 +1,33 @@
 /*
 Big Apples (Weichen Liu + Blueface, Jeffery Tang + Mathias, Lior + Toothless)
 APCS
-lab01 -- Developing a RPG game
-2021-11-22
+lab01(v2) -- Developing subclasses for Protagonist and Monster
+2021-11-23
 time spent: 0.8 hours
 
 DISCO
-- The instance variables in the super class cannot be private if you want to access them in the sub classes. 
+- The instance variables in the super class cannot be private if you want to access them in the sub classes.
 
 QCC
 - Are there other visibility types in Java?
 */
 
 public class Character {
-  public int health;
-  public int strength;
-  public int defense;
-  public double ar;
+  protected int health;
+  protected int strength;
+  protected int defense;
+  protected double ar;
 
   public boolean isAlive() {
     return (health > 0);
   }
 
   public int getDefense() {
-    return defense; 
+    return defense;
   }
 
   public void lowerHP(int damage) {
-    health -= damage; 
+    health -= damage;
   }
 
   public int attack(Character character) {
@@ -39,6 +39,5 @@ public class Character {
     }
     return damage;
   }
-  
-}
 
+}
