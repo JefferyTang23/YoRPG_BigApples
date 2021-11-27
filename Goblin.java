35cc7038a1b0e061bc986eba558d5dcb95752a93
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-11-27 17:56:01 -0500

    Create Goblin.java

---
+++ Goblin.java
@@ -0,0 +1,27 @@
+/*
+Big Apples (Weichen Liu + Blueface, Jeffery Tang + Mathias, Lior + Toothless)
+APCS
+lab01(v2) -- Developing subclasses for Protagonist and Monster
+2021-11-23
+time spent: 0.8 hours
+
+DISCO
+- The instance variables in the super class cannot be private if you want to access them in the sub classes. 
+
+QCC
+- Are there other visibility types in Java?
+*/
+
+public class Goblin extends Monster{
+  
+  public static String about() {
+    return "A weak goblin.";
+  }
+
+  public Goblin() {
+    health = 100;
+    defense = 20;
+    strength = 35; 
+  }
+
+}
