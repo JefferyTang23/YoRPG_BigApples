14d211924a2ec0b8e3e3c9c75bc9de9e0f124c3e
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-11-27 18:08:17 -0500

    Update Goblin.java

--- Goblin.java
+++ Goblin.java
@@ -1,15 +1,19 @@
 /*
 Big Apples (Weichen Liu + Blueface, Jeffery Tang + Mathias, Lior + Toothless)
 APCS
-lab01(v2) -- Developing subclasses for Protagonist and Monster
-2021-11-23
+lab01 -- Developing a RPG Game
+2021-11-25
 time spent: 0.8 hours
 
 DISCO
-- The instance variables in the super class cannot be private if you want to access them in the sub classes. 
+- The instance variables in the super class cannot be private if you want to access them in the sub classes.
+- The Object type can match variable type, or be any subclass of declared variable type. 
+- If a method accepts a superclass, you can also parameter pass its subclasses. 
 
 QCC
 - Are there other visibility types in Java?
+- How do you use both this() and super() in the same method? 
+
 */
 
 public class Goblin extends Monster{
