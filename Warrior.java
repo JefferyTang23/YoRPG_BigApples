d3dde06a53d85fabdcfbf9a6e6b3c092065b6871
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-11-27 17:57:29 -0500

    Create Warrior.java

---
+++ Warrior.java
@@ -0,0 +1,32 @@
+/*
+Big Apples (Weichen Liu + Blueface, Jeffery Tang + Mathias, Lior + Toothless)
+APCS
+lab01(v2) -- Developing subclasses for Protagonist and Monster
+2021-11-23
+time spent: 0.8 hours
+
+DISCO
+- The instance variables in the super class cannot be private if you want to access them in the sub classes.
+
+QCC
+- Are there other visibility types in Java?
+*/
+
+public class Warrior extends Protagonist {
+  public static String about() {
+    return "A protagonist with a good balance of offense and defense.";
+  }
+
+  public Warrior() {
+    health = 600;
+    strength = 100;
+    defense = 40;
+    ar = 0.5;
+    specialPower = 5;
+  }
+
+  public Warrior(String newName) {
+    this();
+    setName(newName);
+  }
+}
